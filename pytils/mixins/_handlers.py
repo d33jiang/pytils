38f8d93aea12c4371a853775e0d36d535f5f38ad
David Jiang <d33jiang@users.noreply.github.com>
2019-05-10 00:10:40 -0400

    Added docstrings to pytils.mixins.DaemonHandler

--- pytils/mixins/_handlers.py
+++ pytils/mixins/_handlers.py
@@ -7,18 +7,36 @@ __all__ = [
 
 
 class DaemonHandler:
+    """
+    Boilerplate class for handler functionality that is intended to be run in a number of daemon threads.
+
+    One daemon thread is spawned for every call to start. Each daemon thread runs a loop in which it checks that the
+    handler is active before proceeding to invoke the handle_one implementation. A daemon thread will terminate if the
+    the handler is inactive when it performs the check.
+
+    The handle_one method must be overridden to attempt to perform one handling action. It is run in a loop; thus, care
+    should be taken so that the daemon threads do not spin as a result of the handle_one implementation.
+
+    The is_active method may be overridden to signal any running daemon threads to gradually terminate while the Python
+    interpreter instance continues to run.
+
+    """
 
     def is_active(self) -> Any:
-        return self
+        """Get whether this handler is active."""
+        return True
 
     def handle_one(self):
+        """Perform one handling action."""
         raise NotImplementedError
 
     def run(self):
+        """Run the handler loop."""
         while self.is_active():
             self.handle_one()
 
     def start(self) -> Thread:
+        """Start a daemon thread to run the handler loop."""
         t = Thread(target=self.run, daemon=True)
         t.start()
 
